175af8f9e1ba340b476e0a27a4ed2520f5392648
Carlos Camilo <carloscamilocontato@gmail.com>
2025-03-06 09:27:09 -0300

    list links

--- app/controllers/social_links_controller.rb
+++ app/controllers/social_links_controller.rb
@@ -1,22 +1,23 @@
 class SocialLinksController < ApplicationController
-  before_action :set_social_link, only: %i[ show update destroy ]
+  before_action :set_social_link, only: %i[ update destroy ]
+  before_action :set_user_social_links, only: :show
   before_action :require_authentication
 
   # GET /social_links
   def index
-    @social_links = SocialLink.all
-
-    render json: @social_links
+    render json: Current.user.social_links
   end
 
   # GET /social_links/1
   def show
-    render json: @social_link
+    render json: @user_social_links
   end
 
   # POST /social_links
   def create
-    @social_link = SocialLink.new(social_link_params)
+    current_user_id = Current.user.id
+    attrs = social_link_params.merge(user_id: current_user_id)
+    @social_link = SocialLink.new(attrs)
 
     if @social_link.save
       render json: @social_link, status: :created, location: @social_link
@@ -44,6 +45,10 @@ class SocialLinksController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_social_link
     @social_link = SocialLink.find(params.expect(:id))
+  end
+
+  def set_user_social_links
+    @user_social_links = SocialLink.where(user_id: params.expect(:id))
   end
 
   # Only allow a list of trusted parameters through.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+  allow_unauthenticated_access only: :create
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { render json: { error: "Too many requests" } }
+
   def create
-    attrs = params.permit(:email_address, :password, :password_confirmation)
+    attrs = params.permit(:email_address, :password)
     user = User.new(attrs)
 
     if user.save
-      render json: { message: "User has been created." }, status: :created
+      start_new_session_for user
+      render json: { message: "User has been created.", user_token: Current.session.token }, status: :created
     else
       render json: { error: user.errors.full_messages }, status: :unprocessable_entity
     end
